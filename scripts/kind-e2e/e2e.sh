ee2b94ff9ad0a1d38212b4545e9492f7ad206615
Tom Pantelis <tompantelis@gmail.com>
2021-04-26 17:11:22 -0400

    E2E benchmark
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- scripts/kind-e2e/e2e.sh
+++ scripts/kind-e2e/e2e.sh
@@ -37,13 +37,19 @@ else
 fi
 
 # run dataplane E2E tests between the two clusters
-${DAPPER_SOURCE}/bin/subctl verify ${verify} --submariner-namespace=$subm_ns --verbose --connection-timeout 20 --connection-attempts 4 \
-    ${KUBECONFIGS_DIR}/kind-config-cluster1 \
-    ${KUBECONFIGS_DIR}/kind-config-cluster2
+#${DAPPER_SOURCE}/bin/subctl verify ${verify} --submariner-namespace=$subm_ns --verbose --connection-timeout 20 --connection-attempts 4 \
+#    ${KUBECONFIGS_DIR}/kind-config-cluster1 \
+#    ${KUBECONFIGS_DIR}/kind-config-cluster2
+#
+#. ${DAPPER_SOURCE}/scripts/kind-e2e/lib_subctl_gather_test.sh
+#
+#test_subctl_gather
 
-. ${DAPPER_SOURCE}/scripts/kind-e2e/lib_subctl_gather_test.sh
+echo "RUNNING benchmark no verbose"
+subctl benchmark latency ${KUBECONFIGS_DIR}/kind-config-cluster1 --intra-cluster
 
-test_subctl_gather
+echo "RUNNING benchmark with verbose"
+subctl benchmark latency ${KUBECONFIGS_DIR}/kind-config-cluster1 --intra-cluster --verbose
 
 print_clusters_message
 
