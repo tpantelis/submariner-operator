181bc4b9ed9b4848f3a2f4dd353eb9ef35e264ab
Tom Pantelis <tompantelis@gmail.com>
2021-05-11 15:52:16 -0400

    Upgrade failure
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/subctl/operator/submarinercr/ensure.go
+++ pkg/subctl/operator/submarinercr/ensure.go
@@ -18,12 +18,16 @@ package submarinercr
 
 import (
 	"context"
+	"time"
 
+	"github.com/pkg/errors"
 	"github.com/submariner-io/admiral/pkg/resource"
-	"github.com/submariner-io/admiral/pkg/util"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/rest"
+	"k8s.io/klog"
 
 	submariner "github.com/submariner-io/submariner-operator/apis/submariner/v1alpha1"
 	submarinerClientset "github.com/submariner-io/submariner-operator/pkg/client/clientset/versioned"
@@ -48,7 +52,7 @@ func Ensure(config *rest.Config, namespace string, submarinerSpec submariner.Sub
 
 	propagationPolicy := metav1.DeletePropagationForeground
 
-	return util.CreateAnew(context.TODO(), &resource.InterfaceFuncs{
+	return createAnew(context.TODO(), &resource.InterfaceFuncs{
 		GetFunc: func(ctx context.Context, name string, options metav1.GetOptions) (runtime.Object, error) {
 			return client.SubmarinerV1alpha1().Submariners(namespace).Get(ctx, name, options)
 		},
@@ -60,5 +64,35 @@ func Ensure(config *rest.Config, namespace string, submarinerSpec submariner.Sub
 		},
 	}, submarinerCR, metav1.CreateOptions{}, metav1.DeleteOptions{
 		PropagationPolicy: &propagationPolicy,
+	})
+}
+
+func createAnew(ctx context.Context, client resource.Interface, obj runtime.Object,
+	createOptions metav1.CreateOptions, deleteOptions metav1.DeleteOptions) error {
+
+	name := resource.ToMeta(obj).GetName()
+
+	backOff := wait.Backoff{
+		Steps:    10,
+		Duration: 500 * time.Millisecond,
+		Factor:   1.5,
+		Cap:      10 * time.Minute,
+	}
+
+	return wait.ExponentialBackoff(backOff, func() (bool, error) {
+		_, err := client.Create(ctx, obj, createOptions)
+		klog.Infof("**Create: %v", err)
+
+		if !apierrors.IsAlreadyExists(err) {
+			return true, err
+		}
+
+		err = client.Delete(ctx, name, deleteOptions)
+		klog.Infof("**Delete: %v", err)
+		if apierrors.IsNotFound(err) {
+			err = nil
+		}
+
+		return false, errors.WithMessagef(err, "failed to delete pre-existing instance %q", name)
 	})
 }
